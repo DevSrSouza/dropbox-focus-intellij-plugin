d293c2d526339a9b7e2eebf97919d626f0dfae34
GitHub Action <action@github.com>
2022-03-30 02:35:47 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "dropbox-focus-intellij-plugin"
---
+++ src/main/kotlin/com/github/devsrsouza/dropboxfocusintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.devsrsouza.dropboxfocusintellijplugin.services
+
+import com.github.devsrsouza.dropboxfocusintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
