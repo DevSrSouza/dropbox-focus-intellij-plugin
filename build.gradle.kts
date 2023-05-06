3d067af946cfbb7926e457f53bbf9225a315b9b6
DevSrSouza <devsrsouza@gmail.com>
2023-05-06 14:44:21 -0300

    chore: disabling qodana

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,6 @@ plugins {
     id("org.jetbrains.intellij") version "1.13.3"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
 
     id("org.jmailen.kotlinter") version "3.9.0"
 
@@ -64,14 +62,6 @@ intellij {
 changelog {
     version.set(properties("pluginVersion"))
     groups.set(emptyList())
-}
-
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
 kotlinter {
